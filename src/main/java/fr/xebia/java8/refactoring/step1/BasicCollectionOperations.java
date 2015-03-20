a310ceb51afa633d69d0e97434afd3b845359fbe
julien <girardot.jul@gmail.com>
2015-03-20 14:27:21 +0100

    step1-end

--- src/main/java/fr/xebia/java8/refactoring/step1/BasicCollectionOperations.java
+++ src/main/java/fr/xebia/java8/refactoring/step1/BasicCollectionOperations.java
@@ -10,6 +10,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.math.BigDecimal.ONE;
+
 public class BasicCollectionOperations {
 
     private static Map<String, BigDecimal> CURRENCIES_BY_ISOCODE = new HashMap<>();
@@ -18,7 +20,7 @@ public class BasicCollectionOperations {
 
     static {
         CURRENCIES_BY_ISOCODE.put("USD", BigDecimal.valueOf(0.91));
-        CURRENCIES_BY_ISOCODE.put("GPB", BigDecimal.valueOf(1.38));
+        CURRENCIES_BY_ISOCODE.put("GBP", BigDecimal.valueOf(1.38));
         CURRENCIES_BY_ISOCODE.put("AUD", BigDecimal.valueOf(0.70));
         CURRENCIES_BY_ISOCODE.put("EUR", BigDecimal.valueOf(1));
 
@@ -27,85 +29,42 @@ public class BasicCollectionOperations {
         FIBONACCI_CACHE.put(2, 1L);
     }
 
-    //TODO :Refactor with forEach method
     public static void resetPassword(List<User> users) {
-
-        for (User user : users) {
-            user.password = null;
-        }
+        users.forEach(user -> user.password = null);
     }
 
-    //TODO :Refactor with removeIf method and use method reference
     public static void removeExpiredUsers(List<User> users) {
-        List<User> usersToRemove = new ArrayList<>();
-
-        for (User user : users) {
-            if (user.isExpired()) {
-                usersToRemove.add(user);
-            }
-        }
-
-        users.removeAll(usersToRemove);
+        users.removeIf(User::isExpired);
     }
 
-    //TODO :Refactor with replaceAll method
     public static void addOneDayToDates(List<LocalDate> localDates) {
-
-        for (int i = 0; i < localDates.size(); i++) {
-            LocalDate localDate = localDates.get(i);
-            LocalDate newDate = localDate.plusDays(1);
-
-            localDates.set(i, newDate);
-        }
+        localDates.replaceAll(date -> date.plusDays(1));
     }
 
-    //TODO :Refactor Map computation with merge method and you can eventually change loop by forEach method
-    //TODO Use the sum method on Integer
     public static Map<String, Integer> countWord(List<String> words) {
         Map<String, Integer> count = new HashMap<>();
 
-        for (String word : words) {
-            Integer countOfCurrentWord = count.get(word);
-            if (countOfCurrentWord == null) {
-                countOfCurrentWord = 0;
-            }
-
-            count.put(word, countOfCurrentWord + 1);
-        }
+        // words.forEach(s -> count.compute(s, (s1, integer) -> count.getOrDefault(s1, 0) + 1));
+        words.forEach(s -> count.merge(s, 1, Integer::sum));
 
         return count;
     }
 
-    //TODO : use getOrDefault Map method
     public static BigDecimal exchangeRateWithEuro(String isoCode) {
-        BigDecimal currencyByIsocode = CURRENCIES_BY_ISOCODE.get(isoCode);
-        if (currencyByIsocode != null) {
-            return currencyByIsocode;
-        }
-
-        return BigDecimal.ONE; //Default Currency is One (no conversion)
+        return CURRENCIES_BY_ISOCODE.getOrDefault(isoCode, ONE);
     }
 
     public static List<Long> fibonacci(int expectedNumberResult) {
         List<Long> result = new ArrayList<>(expectedNumberResult);
 
         for (int i = 1; i <= expectedNumberResult; i++) {
-
             result.add(fibonacciComputation(i));
         }
         return result;
     }
 
-    //TODO: Use computeIfAbsent map method
     private static long fibonacciComputation(int number) {
-        if (FIBONACCI_CACHE.containsKey(number)) {
-            return FIBONACCI_CACHE.get(number);
-        }
-
-        long fibonacci = fibonacciComputation(number - 1) + fibonacciComputation(number - 2);
-
-        FIBONACCI_CACHE.put(number, fibonacci);
-
-        return fibonacci;
+        return FIBONACCI_CACHE.computeIfAbsent(number, n -> fibonacciComputation(n - 1) + fibonacciComputation(n - 2));
     }
+
 }
--- src/test/java/fr/xebia/java8/refactoring/step1/BasicCollectionOperationsTest.java
+++ src/test/java/fr/xebia/java8/refactoring/step1/BasicCollectionOperationsTest.java
@@ -11,60 +11,63 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static fr.xebia.java8.refactoring.step1.BasicCollectionOperations.exchangeRateWithEuro;
+import static fr.xebia.java8.refactoring.step1.BasicCollectionOperations.fibonacci;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BasicCollectionOperationsTest {
 
     @Test
     public void should_empty_password() {
-        //given
+        // Given
         List<User> users = new ArrayList<>();
         users.add(new User("M", "Eddard", "Stark").withPassword("1234").withExpireDate(LocalDate.of(2018, 12, 1)));
         users.add(new User("M", "Eddard", "Robb").withPassword("456"));
         users.add(new User("Me", "Eddard", "Arya").withPassword("789").withExpireDate(LocalDate.of(2012, 12, 1)));
         users.add(new User("M", "Snow", "Jon").withPassword("1011").withExpireDate(LocalDate.of(2011, 12, 1)));
 
-        //when
+        // When
         BasicCollectionOperations.resetPassword(users);
 
-        //then
+        // Then
         assertThat(users).doNotHave(userWithPassword());
-
     }
 
     @Test
     public void should_remove_expired_user() {
-        //given
+        // Given
         List<User> users = new ArrayList<>();
         users.add(new User("M", "Eddard", "Stark").withExpireDate(LocalDate.of(2010, 1, 1)));
         users.add(new User("M", "Eddard", "Robb"));
         users.add(new User("Me", "Eddard", "Arya"));
         users.add(new User("M", "Snow", "Jon"));
 
-        //when
+        // When
         BasicCollectionOperations.removeExpiredUsers(users);
 
-        //then
+        // Then
         assertThat(users).hasSize(3);
         assertThat(users).doNotHave(userExpired());
     }
 
     @Test
     public void should_replace_all_date_by_date_plus_one_day() {
+        //given
         List<LocalDate> localDates = new ArrayList<>();
         localDates.add(LocalDate.of(2014, 2, 10));
         localDates.add(LocalDate.of(2014, 2, 15));
         localDates.add(LocalDate.of(2014, 2, 18));
 
-
+        // When
         BasicCollectionOperations.addOneDayToDates(localDates);
 
+        // Then
         assertThat(localDates).containsExactly(LocalDate.of(2014, 2, 11), LocalDate.of(2014, 2, 16), LocalDate.of(2014, 2, 19));
-
     }
 
     @Test
     public void should_count_words() {
+        // Given
         List<String> words = new ArrayList<>();
         words.add("java");
         words.add("javascript");
@@ -73,27 +76,28 @@ public class BasicCollectionOperationsTest {
         words.add("java");
         words.add("scala");
 
+        // When
         Map<String, Integer> wordCount = BasicCollectionOperations.countWord(words);
 
+        // Then
         assertThat(wordCount).contains(
                 MapEntry.entry("java", 3),
                 MapEntry.entry("javascript", 1),
                 MapEntry.entry("scala", 2));
-
     }
 
     @Test
     public void should_return_exchange_rate() {
-        assertThat(BasicCollectionOperations.exchangeRateWithEuro("EUR")).isEqualByComparingTo("1");
-        assertThat(BasicCollectionOperations.exchangeRateWithEuro("GPB")).isEqualByComparingTo("1.38");
-        assertThat(BasicCollectionOperations.exchangeRateWithEuro("USD")).isEqualByComparingTo("0.91");
-        assertThat(BasicCollectionOperations.exchangeRateWithEuro("AUD")).isEqualByComparingTo("0.70");
-        assertThat(BasicCollectionOperations.exchangeRateWithEuro("AZERTY")).isEqualByComparingTo("1");
+        assertThat(exchangeRateWithEuro("EUR")).isEqualByComparingTo("1");
+        assertThat(exchangeRateWithEuro("GBP")).isEqualByComparingTo("1.38");
+        assertThat(exchangeRateWithEuro("USD")).isEqualByComparingTo("0.91");
+        assertThat(exchangeRateWithEuro("AUD")).isEqualByComparingTo("0.70");
+        assertThat(exchangeRateWithEuro("AZERTY")).isEqualByComparingTo("1");
     }
 
     @Test
     public void should_return_45_fibonacci_number() {
-        assertThat(BasicCollectionOperations.fibonacci(45)).containsExactly(1L, 1L, 2L, 3L, 5L, 8L, 13L, 21L, 34L, 55L, 89L, 144L, 233L, 377L, 610L, 987L, 1597L, 2584L, 4181L, 6765L, 10946L, 17711L, 28657L, 46368L, 75025L, 121393L, 196418L, 317811L, 514229L, 832040L, 1346269L, 2178309L, 3524578L, 5702887L, 9227465L, 14930352L, 24157817L, 39088169L, 63245986L, 102334155L, 165580141L, 267914296L, 433494437L, 701408733L, 1134903170L);
+        assertThat(fibonacci(45)).containsExactly(1L, 1L, 2L, 3L, 5L, 8L, 13L, 21L, 34L, 55L, 89L, 144L, 233L, 377L, 610L, 987L, 1597L, 2584L, 4181L, 6765L, 10946L, 17711L, 28657L, 46368L, 75025L, 121393L, 196418L, 317811L, 514229L, 832040L, 1346269L, 2178309L, 3524578L, 5702887L, 9227465L, 14930352L, 24157817L, 39088169L, 63245986L, 102334155L, 165580141L, 267914296L, 433494437L, 701408733L, 1134903170L);
     }
 
     private Condition<User> userExpired() {
